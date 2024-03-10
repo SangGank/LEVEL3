a03a5c5c1f29ec47aaa264377dc6cc0ea330017c
sanggang <leesk9663@gmail.com>
2024-03-10 21:02:25 +0900

    custom gpt2

--- customModel.py
+++ customModel.py
@@ -1,11 +1,11 @@
 from tqdm import tqdm
 from transformers import (BertForSequenceClassification, BertModel, BertPreTrainedModel, AutoTokenizer,
                           AutoConfig, RobertaPreTrainedModel, RobertaModel, ElectraPreTrainedModel,
-                          ElectraModel)
+                          ElectraModel, GPT2PreTrainedModel, GPT2Model)
 from torch import nn
 from torch.nn import CrossEntropyLoss, MSELoss
 from typing import List, Optional, Tuple, Union
-from transformers.modeling_outputs import SequenceClassifierOutput
+from transformers.modeling_outputs import SequenceClassifierOutput, SequenceClassifierOutputWithPast
 from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
 from transformers.file_utils import (
     ModelOutput,
@@ -481,7 +481,7 @@ class customElectraForSequenceClassification(BertPreTrainedModel):
         if loss1 and loss2 and loss3:
             loss = loss1 + loss2 + loss3
         if not return_dict:
-            output = (logits1,) + (logits2,) + (logits3,) + outputs[2:]
+            output = (logits1,) + (logits2,) + (logits3,) + outputs[1:]
             return ((loss,) + output) if loss is not None else output
         return SequenceClassifierOutput(
             loss=loss,
@@ -490,6 +490,181 @@ class customElectraForSequenceClassification(BertPreTrainedModel):
             attentions=outputs.attentions,
         )
     
+
+class customGPT2ForSequenceClassification(GPT2PreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)
+        self.num_labels1 = config.num_labels1
+        self.num_labels2 = config.num_labels2
+        self.num_labels3 = config.num_labels3
+        self.config = config
+        self.GPT2 = GPT2Model(config)
+        self.classifier1 = nn.Linear(config.hidden_size, self.num_labels1)
+        self.classifier2 = nn.Linear(config.hidden_size, self.num_labels2)
+        self.classifier3 = nn.Linear(config.hidden_size, self.num_labels3)
+
+
+        # Initialize weights and apply final processing
+        self.post_init()
+
+    def forward(
+        self,
+        input_ids: Optional[torch.LongTensor] = None,
+        past_key_values: Optional[Tuple[Tuple[torch.Tensor]]] = None,
+        attention_mask: Optional[torch.FloatTensor] = None,
+        token_type_ids: Optional[torch.LongTensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        head_mask: Optional[torch.FloatTensor] = None,
+        inputs_embeds: Optional[torch.FloatTensor] = None,
+        labels1: Optional[torch.LongTensor] = None,
+        labels2: Optional[torch.LongTensor] = None,
+        labels3: Optional[torch.LongTensor] = None,
+        use_cache: Optional[bool] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+    ) -> Union[Tuple, SequenceClassifierOutputWithPast]:
+        r"""
+        labels (`torch.LongTensor` of shape `(batch_size,)`, *optional*):
+            Labels for computing the sequence classification/regression loss. Indices should be in `[0, ...,
+            config.num_labels - 1]`. If `config.num_labels == 1` a regression loss is computed (Mean-Square loss), If
+            `config.num_labels > 1` a classification loss is computed (Cross-Entropy).
+        """
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        transformer_outputs = self.GPT2(
+            input_ids,
+            past_key_values=past_key_values,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            use_cache=use_cache,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+        hidden_states = transformer_outputs[0]
+        
+        logits1 = self.classifier1(hidden_states)
+        logits2 = self.classifier2(hidden_states)
+        logits3 = self.classifier3(hidden_states)
+
+
+        if input_ids is not None:
+            batch_size, sequence_length = input_ids.shape[:2]
+        else:
+            batch_size, sequence_length = inputs_embeds.shape[:2]
+
+        assert (
+            self.config.pad_token_id is not None or batch_size == 1
+        ), "Cannot handle batch sizes > 1 if no padding token is defined."
+        if self.config.pad_token_id is None:
+            sequence_lengths = -1
+        else:
+            if input_ids is not None:
+                # if no pad token found, use modulo instead of reverse indexing for ONNX compatibility
+                sequence_lengths = torch.eq(input_ids, self.config.pad_token_id).int().argmax(-1) - 1
+                sequence_lengths = sequence_lengths % input_ids.shape[-1]
+                sequence_lengths = sequence_lengths.to(logits.device)
+            else:
+                sequence_lengths = -1
+                # logger.warning(
+                #     f"{self.__class__.__name__} will not detect padding tokens in `inputs_embeds`. Results may be "
+                #     "unexpected if using padding tokens in conjunction with `inputs_embeds.`"
+                # )
+
+        pooled_logits = logits1[torch.arange(batch_size, device=logits1.device), sequence_lengths]
+        pooled_logits = logits2[torch.arange(batch_size, device=logits2.device), sequence_lengths]
+        pooled_logits = logits3[torch.arange(batch_size, device=logits3.device), sequence_lengths]
+
+        
+        loss1 = None
+        loss2 = None
+        loss3 = None
+        if labels1 is not None:
+            if self.config.problem_type is None:
+                if self.num_labels1 == 1:
+                    self.config.problem_type = "regression"
+                elif self.num_labels1 > 1 and (labels1.dtype == torch.long or labels1.dtype == torch.int):
+                    self.config.problem_type = "single_label_classification"
+                else:
+                    self.config.problem_type = "multi_label_classification"
+
+            if self.config.problem_type == "regression":
+                loss_fct = MSELoss()
+                if self.num_labels1 == 1:
+                    loss1 = loss_fct(logits1.squeeze(), labels1.squeeze())
+                else:
+                    loss1 = loss_fct(logits1, labels1)
+            elif self.config.problem_type == "single_label_classification":
+                loss_fct = CrossEntropyLoss()
+                loss1 = loss_fct(logits1.view(-1, self.num_labels1), labels1.view(-1))
+            elif self.config.problem_type == "multi_label_classification":
+                loss_fct = BCEWithLogitsLoss()
+                loss1 = loss_fct(logits1, labels1)
+
+        if labels2 is not None:
+            if self.config.problem_type is None:
+                if self.num_labels2 == 1:
+                    self.config.problem_type = "regression"
+                elif self.num_labels2 > 1 and (labels2.dtype == torch.long or labels2.dtype == torch.int):
+                    self.config.problem_type = "single_label_classification"
+                else:
+                    self.config.problem_type = "multi_label_classification"
+
+            if self.config.problem_type == "regression":
+                loss_fct = MSELoss()
+                if self.num_labels2 == 1:
+                    loss2 = loss_fct(logits2.squeeze(), labels2.squeeze())
+                else:
+                    loss2 = loss_fct(logits2, labels2)
+            elif self.config.problem_type == "single_label_classification":
+                loss_fct = CrossEntropyLoss()
+                loss2 = loss_fct(logits2.view(-1, self.num_labels2), labels2.view(-1))
+            elif self.config.problem_type == "multi_label_classification":
+                loss_fct = BCEWithLogitsLoss()
+                loss2 = loss_fct(logits2, labels2)
+        
+        if labels3 is not None:
+            if self.config.problem_type is None:
+                if self.num_labels3 == 1:
+                    self.config.problem_type = "regression"
+                elif self.num_labels3 > 1 and (labels3.dtype == torch.long or labels3.dtype == torch.int):
+                    self.config.problem_type = "single_label_classification"
+                else:
+                    self.config.problem_type = "multi_label_classification"
+
+            if self.config.problem_type == "regression":
+                loss_fct = MSELoss()
+                if self.num_labels3 == 1:
+                    loss3 = loss_fct(logits3.squeeze(), labels3.squeeze())
+                else:
+                    loss3 = loss_fct(logits3, labels3)
+            elif self.config.problem_type == "single_label_classification":
+                loss_fct = CrossEntropyLoss()
+                loss3 = loss_fct(logits3.view(-1, self.num_labels3), labels3.view(-1))
+            elif self.config.problem_type == "multi_label_classification":
+                loss_fct = BCEWithLogitsLoss()
+                loss3 = loss_fct(logits3, labels3)
+        loss =None
+        if loss1 and loss2 and loss3:
+            loss = loss1 + loss2 + loss3
+            
+        if not return_dict:
+            output = (pooled_logits,) + transformer_outputs[1:]
+            return ((loss,) + output) if loss is not None else output
+
+        return SequenceClassifierOutputWithPast(
+            loss=loss,
+            logits=pooled_logits,
+            past_key_values=transformer_outputs.past_key_values,
+            hidden_states=transformer_outputs.hidden_states,
+            attentions=transformer_outputs.attentions,
+        )
+
+
 
 
 
